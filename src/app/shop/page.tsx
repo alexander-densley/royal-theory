53244233fe5c8c1df959fd0efefd3f3907195868
Alexander Densley <densley.alexander@gmail.com>
2024-07-31 20:48:17 -0600

    grid update

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -72,7 +72,7 @@ const images = [
 export default function ShopPage() {
 	return (
 		<div className='flex items-center justify-center min-h-screen my-8'>
-			<div className='grid grid-cols-3 gap-4'>
+			<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
 				{images.map((image, index) => (
 					<div key={index} className='flex flex-col'>
 						<Image
