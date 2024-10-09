cfa729f744504de541aef32cb03ba22993f88f40
Alexander Densley <densley.alexander@gmail.com>
2024-10-08 20:55:50 -0600

    navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,11 +4,12 @@ import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
-import { ShoppingCart } from 'lucide-react'
+import { ShoppingCart, ArrowLeft } from 'lucide-react'
 export default function NavBar() {
 	const pathname = usePathname()
 	const isShop = pathname.startsWith('/shop')
 	const isContact = pathname.startsWith('/contact')
+	const isShopIdPage = pathname.startsWith('/shop/')
 
 	return (
 		<div className='flex flex-col w-full items-center justify-center pt-4'>
@@ -32,24 +33,37 @@ export default function NavBar() {
 				/>
 			</Link>
 
-			<div className='flex w-full justify-center gap-24 sm:gap-52 mt-16'>
-				<Link
-					href='/shop'
-					className={`text-2xl font-semibold hover:text-[#B7C8D7] ${
-						isShop ? 'text-[#B7C8D7]' : ''
-					}`}
-				>
-					SHOP
-				</Link>
-				<Link
-					href='/contact'
-					className={`text-2xl font-semibold hover:text-[#B7C8D7] ${
-						isContact ? 'text-[#B7C8D7]' : ''
-					}`}
-				>
-					CONTACT
-				</Link>
-			</div>
+			{(isShop || isContact) && !isShopIdPage ? (
+				<div className='flex w-full justify-center gap-24 sm:gap-52 mt-16'>
+					<Link
+						href='/shop'
+						className={`text-2xl font-semibold hover:text-[#B7C8D7] ${
+							isShop ? 'text-[#B7C8D7]' : ''
+						}`}
+					>
+						SHOP
+					</Link>
+					<Link
+						href='/contact'
+						className={`text-2xl font-semibold hover:text-[#B7C8D7] ${
+							isContact ? 'text-[#B7C8D7]' : ''
+						}`}
+					>
+						CONTACT
+					</Link>
+				</div>
+			) : (
+				<div className='flex w-full justify-center gap-24 sm:gap-52 mt-16'>
+					<Link
+						href='/shop'
+						className='text-2xl font-semibold hover:text-[#B7C8D7]'
+					>
+						<div className='flex items-center gap-2'>
+							<ArrowLeft size={24} /> BACK TO SHOP
+						</div>
+					</Link>
+				</div>
+			)}
 		</div>
 	)
 }
