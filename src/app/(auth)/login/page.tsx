712cc5de100d48822c84de6ac7655aa51f255a89
Alexander Densley <densley.alexander@gmail.com>
2025-02-01 21:27:16 -0800

    remove login

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -3,7 +3,6 @@ import { Button } from '@/components/ui/button'
 import { Card, CardHeader, CardContent, CardFooter } from '@/components/ui/card'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
-import Link from 'next/link'
 export default function LoginPage() {
 	return (
 		<div className='min-h-screen flex items-center justify-center px-4'>
@@ -41,11 +40,6 @@ export default function LoginPage() {
 						<Button className='w-full' formAction={login}>
 							Log in
 						</Button>
-						<Link href='/signup'>
-							<Button className='w-full' variant='outline'>
-								Sign up
-							</Button>
-						</Link>
 					</CardFooter>
 				</form>
 			</Card>
