bb3e1d13dc54297e052a214277cd5547a2b0d554
Alexander Densley <densley.alexander@gmail.com>
2025-02-01 20:40:41 -0800

    remove some console logs

--- src/app/api/get-payment-link/route.ts
+++ src/app/api/get-payment-link/route.ts
@@ -1,16 +1,13 @@
 import { NextRequest, NextResponse } from 'next/server'
-import { createClient } from '@/utils/supabase/server'
 import Stripe from 'stripe'
 
 export async function POST(req: NextRequest) {
 	const { line_items } = await req.json()
 	console.log(line_items)
-	console.log(process.env.STRIPE_SECRET_KEY as string)
 	const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
 	const paymentLink = await stripe.paymentLinks.create({
 		line_items,
 	})
-	console.log(paymentLink)
 	return NextResponse.json({ url: paymentLink.url }, { status: 200 })
 }
