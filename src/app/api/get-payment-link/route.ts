f826da6f7a557efb4c6adf7439fbf67f928a1e5d
Alexander Densley <densley.alexander@gmail.com>
2025-02-25 13:13:14 -0800

    meta2

--- src/app/api/get-payment-link/route.ts
+++ src/app/api/get-payment-link/route.ts
@@ -1,13 +1,18 @@
 import { NextRequest, NextResponse } from 'next/server'
 import Stripe from 'stripe'
-
+import { CartProduct } from '@/stores/cart-store'
 export async function POST(req: NextRequest) {
-	const { line_items } = await req.json()
+	const { line_items, metadata } = await req.json()
 	console.log(line_items)
 	const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string)
 
 	const paymentLink = await stripe.paymentLinks.create({
 		line_items,
+		metadata: {
+			products: metadata.products.map((product: CartProduct) => ({
+				...product,
+			})),
+		},
 		shipping_address_collection: {
 			allowed_countries: ['US'],
 		},
